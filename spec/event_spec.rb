a98c48fefbb2469a5f9c875388e2b0aae71cc2ca
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-20 14:04:29 -0600

    Create test for total quantity heklper method

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -133,4 +133,28 @@ RSpec.describe Event do
       expect(event.overstocked_items).to eq([item_1])
     end
   end
+
+  describe '#total_quantity' do
+    it 'returns total quantity of given item over all trucks' do
+      event = Event.new('South Pearl Street Farmers Market')
+      food_truck_1 = FoodTruck.new('Rocky Mountain Pies')
+      item_1 = Item.new(name: 'Peach Pie (Slice)', price: '$3.75')
+      item_2 = Item.new(name: 'Apple Pie (Slice)', price: '$2.50')
+      item_3 = Item.new(name: 'Peach-Raspberry Nice Cream', price: '$5.30')
+      item_4 = Item.new(name: 'Banana Nice Creamm', price: '$4.25')
+      food_truck_2 = FoodTruck.new('Ba-Nom-a-Nom')
+      food_truck_3 = FoodTruck.new('Palisade Peach Shack')
+
+      food_truck_1.stock(item_1, 35)
+      food_truck_1.stock(item_2, 7)
+      food_truck_2.stock(item_3, 25)
+      food_truck_2.stock(item_4, 50)
+      food_truck_3.stock(item_1, 65)
+      event.add_food_truck(food_truck_1)
+      event.add_food_truck(food_truck_2)
+      event.add_food_truck(food_truck_3)
+
+      expect(event.total_quantity(item_1)).to eq(100)
+    end
+  end
 end
