3daea9fc7f3ee54268a6d3f5a9543bcdc21433e6
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-20 13:26:47 -0600

    Expanded on stock test to ensure the items were being saved to inventory

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -36,12 +36,22 @@ RSpec.describe FoodTruck do
     it 'stocks a given quantity of an item' do
       food_truck = FoodTruck.new('Rocky Mountain Pies')
       item_1 = Item.new(name: 'Peach Pie (Slice)', price: '$3.75')
+      item_2 = Item.new(name: 'Apple Pie (Slice)', price: '$2.50')
 
       expect(food_truck.check_stock(item_1)).to eq(0)
 
       food_truck.stock(item_1, 30)
 
       expect(food_truck.check_stock(item_1)).to eq(30)
+
+      food_truck.stock(item_2, 15)
+
+      expected = {
+        item_1 => 30,
+        item_2 => 15
+      }
+
+      expect(food_truck.inventory).to eq(expected)
     end
-  end 
+  end
 end
