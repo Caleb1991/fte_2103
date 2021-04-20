99559ac69dbf2235669e8dcb605c86d5c579fb76
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-20 13:51:03 -0600

    Create potential revenue test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -54,4 +54,26 @@ RSpec.describe FoodTruck do
       expect(food_truck.inventory).to eq(expected)
     end
   end
+
+  describe '#potential_revenue' do
+    it 'returns potential revenue for truck' do
+      food_truck_1 = FoodTruck.new('Rocky Mountain Pies')
+      item_1 = Item.new(name: 'Peach Pie (Slice)', price: '$3.75')
+      item_2 = Item.new(name: 'Apple Pie (Slice)', price: '$2.50')
+      item_3 = Item.new(name: 'Peach-Raspberry Nice Cream', price: '$5.30')
+      item_4 = Item.new(name: 'Banana Nice Creamm', price: '$4.25')
+      food_truck_2 = FoodTruck.new('Ba-Nom-a-Nom')
+      food_truck_3 = FoodTruck.new('Palisade Peach Shack')
+
+      food_truck_1.stock(item_1, 35)
+      food_truck_1.stock(item_2, 7)
+      food_truck_2.stock(item_3, 25)
+      food_truck_2.stock(item_4, 50)
+      food_truck_3.stock(item_1, 65)
+
+      expect(food_truck_1.potential_revenue).to eq(148.75)
+      expect(food_truck_2.potential_revenue).to eq(345.00)
+      expect(food_truck_3.potential_revenue).to eq(243.75)
+    end
+  end
 end
