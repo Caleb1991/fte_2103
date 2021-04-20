c186aef14cf36facb33d1172e785dd3ccfc49c70
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-20 13:57:24 -0600

    Create test for sorted item list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -83,4 +83,30 @@ RSpec.describe Event do
       expect(event.food_trucks_that_sell(item_1)).to eq(expected)
     end
   end
+
+  describe '#sorted_item_list' do
+    it 'returns all items stocked by food trucks alphabetically' do
+      event = Event.new('South Pearl Street Farmers Market')
+      food_truck_1 = FoodTruck.new('Rocky Mountain Pies')
+      item_1 = Item.new(name: 'Peach Pie (Slice)', price: '$3.75')
+      item_2 = Item.new(name: 'Apple Pie (Slice)', price: '$2.50')
+      item_3 = Item.new(name: 'Peach-Raspberry Nice Cream', price: '$5.30')
+      item_4 = Item.new(name: 'Banana Nice Creamm', price: '$4.25')
+      food_truck_2 = FoodTruck.new('Ba-Nom-a-Nom')
+      food_truck_3 = FoodTruck.new('Palisade Peach Shack')
+
+      food_truck_1.stock(item_1, 35)
+      food_truck_1.stock(item_2, 7)
+      food_truck_2.stock(item_3, 25)
+      food_truck_2.stock(item_4, 50)
+      food_truck_3.stock(item_1, 65)
+      event.add_food_truck(food_truck_1)
+      event.add_food_truck(food_truck_2)
+      event.add_food_truck(food_truck_3)
+
+      expected = ['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream']
+
+      expect(event.sorted_item_list).to eq(expected)
+    end
+  end
 end
