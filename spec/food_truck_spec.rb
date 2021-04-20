2916363e08256b68a7e8d490fde66dc2abdec228
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-20 13:14:22 -0600

    Create initialize tests

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,24 @@
+require './lib/food_truck'
+require './lib/item'
+
+RSpec.describe FoodTruck do
+
+  describe 'initialize' do
+    it 'exists' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+
+      expect(food_truck).to be_an_instance_of(FoodTruck)
+    end
+
+    it 'has a name' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+
+      expect(food_truck.name).to eq('Rocky Mountain Pies')
+    end
+
+    it 'has no inventory by default' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+
+      expect(food_truck.inventory).to eq([])
+    end
+  end
